e4014b49ba04e45c2333763b231462af8d987db2
joelc <joelc@DESKTOP-BBCLB48>
2017-01-17 18:20:38 -0600

    Tweaks to console messages and commenting to clarify processing

--- Letterer/LettererMain.java
+++ Letterer/LettererMain.java
@@ -3,19 +3,29 @@ import java.util.Scanner;
 public class LettererMain {
 
 	public static Letters letters;
+	public final static String EXIT = "[exit]";
 
 	public static void main(String[] args) {
 
 		System.out.println("WELCOME TO THE LETTERER!\n\n"+
 				"TYPE IN YOUR MESSAGE AND WE WILL CONVERT IT INTO JUMBO LETTERS!");
-
+		System.out.println("\ntype \"[exit]\" to exit program");
+		
 		initialSetup();
+		
+		boolean done = false;
 
-		while(true){
+		while(!done){
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("\n\nEnter your message: ");
 			String message = scanner.next();
-			StringProcessor.convertString(message);
+			if(!EXIT.equals(message)){
+				StringProcessor.convertString(message);
+			}
+			else {
+				scanner.close();
+				done = true;
+			}
 		}
 	}
 
--- Letterer/StringProcessor.java
+++ Letterer/StringProcessor.java
@@ -2,11 +2,16 @@
 public class StringProcessor {
 
 	public static void convertString(String message){
+		//gets array of letters from user input
 		Letter[] msgArray = new Letter[message.length()];
 		for(int i = 0; i < message.length(); i++){
 			msgArray[i] = LetterProcessor.getLetter(message.charAt(i));
 		}
 		
+		//uses the leading and trailing matrices to massage spacing of each letter so it fits with adjacent letters
+		//TODO
+		
+		//creates and prints the word line by line
 		for(int i = 0; i < 7; i++){
 			String line = "";
 			for(int j = 0; j < message.length(); j++){
